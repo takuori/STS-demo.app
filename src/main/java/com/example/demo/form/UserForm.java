72d9d7986f1f2680341b001273fe0b8fc89b8468
MCZ-s <104710179+takuori@users.noreply.github.com>
2022-09-15 18:50:16 +0900

    [5thCommit]バリデーション設定＊importがみつからない状態

--- src/main/java/com/example/demo/form/UserForm.java
+++ src/main/java/com/example/demo/form/UserForm.java
@@ -1,10 +1,16 @@
 package com.example.demo.form;
 
+import javax.validation.constraints.Size;
+
 import lombok.Data;
 
 @Data
 public class UserForm {
+	
+	@Size(min = 1, max = 200)
 	private String name;
+	
+	
 	private String email;
 
 }
