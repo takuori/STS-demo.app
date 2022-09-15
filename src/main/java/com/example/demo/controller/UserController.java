edde3946cfcf277043c38b83e57f99305dd26996
MCZ-s <104710179+takuori@users.noreply.github.com>
2022-09-15 17:44:44 +0900

    [4thCommit]新規登録機能追加

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.data.entity.User;
 import com.example.demo.data.repository.UserRepository;
+import com.example.demo.form.UserForm;
 
 @Controller
 public class UserController {
@@ -21,6 +23,28 @@ public class UserController {
 		List<User> users = userRepository.findAll();
 		model.addAttribute("users", users);
 		return "users";
+	}
+	
+	@GetMapping("/newuser") 
+	//"/newuser"に対してHTTPのGETメソッドを受け付けるための設定
+	
+    public String getNewUser(Model model) {
+		//引数で受け取ったModelのインスタンスに、35行目で新しくインスタンス化したUserFormを渡す
+		//これにより、HTMLテンプレートから値をセットするための設定ができた
+		
+		UserForm userForm = new UserForm();
+		model.addAttribute("userForm", userForm);
+    	return "newuser";
+    }
+	
+	@PostMapping("/newuser")
+	public String registerUser(UserForm userForm) {
+		User user = new User();
+		user.setName(userForm.getName());
+		user.setEmail(userForm.getEmail());
+		
+		userRepository.save(user);
+		return "redirect:/users";
 	}
 
 }
